c70ff30f0ad835c69aed120d2c4766561c269fb7
EssieFir <65592600+EssieFir@users.noreply.github.com>
2024-03-04 06:10:47 +1000

    Fix Infinite Eat Glitch

--- src/main/java/essie/beefchunkus/BeefChunkusEvent.java
+++ src/main/java/essie/beefchunkus/BeefChunkusEvent.java
@@ -19,39 +19,79 @@ import java.util.Objects;
 
 public class BeefChunkusEvent implements Listener {
 
+    private static boolean isItemBeefChunkus(ItemStack item) {
+        if (item != null && item.hasItemMeta() && item.getItemMeta().hasCustomModelData()) {
+            if (item.getItemMeta().getCustomModelData() == BeefChunkusItem.beefChunkus.getItemMeta().getCustomModelData()) {
+                if (item.getType().equals(BeefChunkusItem.beefChunkus.getType())) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+
+    private static void damageItemAndFeedPlayer(Player player) {
+        ItemStack item;
+        boolean isItemInMainHand = true;
+
+        //Check each hand if the item is Beef Chunkus
+        if (isItemBeefChunkus(player.getInventory().getItemInMainHand())) {
+            item = player.getInventory().getItemInMainHand();
+        } else if (isItemBeefChunkus(player.getInventory().getItemInOffHand())) {
+            item = player.getInventory().getItemInOffHand();
+            isItemInMainHand = false;
+        } else {
+            item = new ItemStack(Material.AIR);
+        }
+
+        //Get the Damage metadata (tag)
+        Damageable itemdmg = (Damageable) item.getItemMeta();
+        assert itemdmg != null;
+        int damage;
+
+        //Damage the item (less than 4 check is, so it adds up to 10 uses)
+        if (itemdmg.getDamage() <= 4) {
+            damage = itemdmg.getDamage() + 2;
+        } else {
+            damage = itemdmg.getDamage() + 3;
+        }
+        itemdmg.setDamage((short) damage);
+
+        //Damage the item
+        if (!player.getGameMode().equals(GameMode.CREATIVE)) {
+            item.setItemMeta(itemdmg);
+        }
+
+        //Feed the player
+        player.setSaturation(player.getSaturation() + 10f);
+        player.setFoodLevel(player.getFoodLevel() + 7);
+
+        //"Break" the item if it's over 25 damage
+        if (itemdmg.getDamage() >= 25) {
+            item.setType(Material.AIR);
+        }
+
+        //Spawn particles and sounds
+        player.spawnParticle(Particle.ITEM_CRACK, player.getLocation().add(0, 1.6, 0), 10, 0, 0, 0, 0.1, BeefChunkusItem.beefChunkus);
+        player.playSound(player, Sound.ENTITY_GENERIC_EAT, 1, 1);
+
+        //Replace the item in the players respective hand, main hand prioritized.
+        if (isItemInMainHand) {
+            player.getInventory().setItemInMainHand(item);
+        }
+        else {
+            player.getInventory().setItemInOffHand(item);
+        }
+    }
+
     @EventHandler
     public static void onRightClick(PlayerInteractEvent event) {
         if (event.getAction() == Action.RIGHT_CLICK_AIR || event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            if (event.getItem() != null && event.getItem().hasItemMeta() && event.getItem().getItemMeta().hasCustomModelData()) {
-                if (event.getItem().getItemMeta().getCustomModelData() == BeefChunkusItem.beefChunkus.getItemMeta().getCustomModelData()) {
-                    if (event.getItem().getType().equals(BeefChunkusItem.beefChunkus.getType())) {
-                        Player player = event.getPlayer();
-                        ItemStack item = event.getItem();
-                        Damageable itemdmg = (Damageable) item.getItemMeta();
-                        assert itemdmg != null;
-                        int damage;
-                        if (itemdmg.getDamage() <= 4) {
-                            damage = itemdmg.getDamage() + 2;
-                        } else {
-                            damage = itemdmg.getDamage() + 3;
-                        }
-                        itemdmg.setDamage((short) damage);
-
-                        if (!player.getGameMode().equals(GameMode.CREATIVE)) {
-                            item.setItemMeta(itemdmg);
-                        }
-
-                        player.setSaturation(player.getSaturation() + 10f);
-                        player.setFoodLevel(player.getFoodLevel() + 7);
-
-                        if (itemdmg.getDamage() >= 25) {
-                            item.setType(Material.AIR);
-                        }
-
-                        player.spawnParticle(Particle.ITEM_CRACK, player.getLocation().add(0,1.6,0), 10, 0, 0, 0, 0.1, BeefChunkusItem.beefChunkus);
-                        player.playSound(player, Sound.ENTITY_GENERIC_EAT, 1, 1);
-                        player.getInventory().setItemInMainHand(item);
-                    }
+            if (isItemBeefChunkus(event.getItem())) {
+                Player player = event.getPlayer();
+                if (player.getFoodLevel() <= 19 || player.getGameMode().equals(GameMode.CREATIVE)) {
+                    damageItemAndFeedPlayer(player);
                 }
             }
         }
