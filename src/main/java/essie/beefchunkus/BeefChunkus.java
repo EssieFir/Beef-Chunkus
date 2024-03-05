57cde3e0c28fb360d31927b341fcc8fd6b2b5751
EssieFir <65592600+EssieFir@users.noreply.github.com>
2024-03-06 05:27:08 +1000

    Overhaul Beef Chunkus

--- src/main/java/essie/beefchunkus/BeefChunkus.java
+++ src/main/java/essie/beefchunkus/BeefChunkus.java
@@ -28,11 +28,11 @@ public class BeefChunkus {
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, MOD_ID);
     // Create a Deferred Register to hold Items which will all be registered under the "beef_chunkus" namespace
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
-    public static final FoodProperties BEEF_CHUNKUS_FOOD = (new FoodProperties.Builder()).nutrition(7).saturationMod(10F).build();
+    public static final FoodProperties BEEF_CHUNKUS_FOOD = (new FoodProperties.Builder()).build();
 
     // Creates a new Block with the id "beef_chunkus:example_block", combining the namespace and path
     // Creates a new BlockItem with the id "beef_chunkus:example_block", combining the namespace and path
-    public static final RegistryObject<Item> BEEF_CHUNKUS = ITEMS.register("beef_chunkus", () -> new BeefChunkusItem(new Item.Properties().durability(10).food(BEEF_CHUNKUS_FOOD).tab(CreativeModeTab.TAB_FOOD)));
+    public static final RegistryObject<Item> BEEF_CHUNKUS = ITEMS.register("beef_chunkus", () -> new BeefChunkusItem(new Item.Properties().durability(100).food(BEEF_CHUNKUS_FOOD).tab(CreativeModeTab.TAB_FOOD)));
 
     public BeefChunkus() {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
--- src/main/java/essie/beefchunkus/BeefChunkusItem.java
+++ src/main/java/essie/beefchunkus/BeefChunkusItem.java
@@ -1,32 +1,144 @@
 package essie.beefchunkus;
 
+import net.minecraft.ChatFormatting;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.Level;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.List;
 
 public class BeefChunkusItem extends Item {
+
+    private static final String FOOD_NOTCHES_TAG = "beef_chunkus:food_notches";
+    private static final String MAX_FOOD_NOTCHES_TAG = "beef_chunkus:max_food_notches";
+
     public BeefChunkusItem(Properties properties) {
         super(properties);
+    }
+    @Override
+    public boolean isEnchantable(ItemStack pStack) { return false; }
+    @Override
+    public boolean isRepairable(ItemStack pStack) { return false; }
+
+    @Override
+    public void appendHoverText(ItemStack pStack, @Nullable Level pLevel, List<Component> pTooltip, TooltipFlag pContext) {
+
+        CompoundTag compoundTag = pStack.getTag();
+
+        //Check if the item has the custom nbt data, if not, add it.
+        if (compoundTag.get(FOOD_NOTCHES_TAG) == null) {
+            setTag(pStack, FOOD_NOTCHES_TAG, 70);
+        }
+        if (compoundTag.get(MAX_FOOD_NOTCHES_TAG) == null) {
+            setTag(pStack, MAX_FOOD_NOTCHES_TAG, 70);
+        }
+
+        int remainingFoodNotches = compoundTag.getInt(FOOD_NOTCHES_TAG);
+        int maxFoodNotches = compoundTag.getInt(MAX_FOOD_NOTCHES_TAG);
+
+        pTooltip.add(Component.translatable("tooltip.beef_chunkus.remaining_notches",
+                (Component.literal(String.valueOf(remainingFoodNotches))), Component.literal(String.valueOf(maxFoodNotches))).withStyle(ChatFormatting.GRAY));
     }
 
     @Override
     public ItemStack finishUsingItem(ItemStack pStack, Level pLevel, LivingEntity pEntityLiving) {
-        //Get the durability of the item before it is consumed.
-        ItemStack initialItem = pStack.copy();
 
+        //itemstack is consumed and set to air when passed through finishUsingItem, so copy it before it gets deleted.
+        ItemStack initialItem = pStack.copy();
         ItemStack itemstack = super.finishUsingItem(pStack, pLevel, pEntityLiving);
         ItemStack item = new ItemStack(BeefChunkus.BEEF_CHUNKUS.get());
-        boolean isCreative = ((Player) pEntityLiving).getAbilities().instabuild;
 
         item.setTag(initialItem.getTag());
-        item.setDamageValue(initialItem.getDamageValue() + 1);
 
-        if (item.getDamageValue() >= 10 || isCreative) {
-            return itemstack;
+        if (pEntityLiving instanceof Player player) {
+
+            boolean isCreative = player.getAbilities().instabuild;
+
+            CompoundTag initialTag = initialItem.getTag();
+
+            //Check if the item has the custom nbt data, if not, add it.
+            if (initialTag.get(FOOD_NOTCHES_TAG) == null) {
+                setTag(item, FOOD_NOTCHES_TAG, 70);
+            }
+            if (initialTag.get(MAX_FOOD_NOTCHES_TAG) == null) {
+                setTag(item, MAX_FOOD_NOTCHES_TAG, 70);
+            }
+
+            int remainingFoodNotches = initialTag.getInt(FOOD_NOTCHES_TAG);
+            int maxFoodNotches = initialTag.getInt(MAX_FOOD_NOTCHES_TAG);
+
+            int amountToFeed = 20 - player.getFoodData().getFoodLevel();
+            float amountToSaturate;
+
+            if (isCreative) {
+                player.getFoodData().setFoodLevel(20);
+                player.getFoodData().setSaturation(20);
+            } else {
+                //Feed the player
+                int playerFoodLevel = player.getFoodData().getFoodLevel();
+                float playerSaturationLevel = player.getFoodData().getSaturationLevel();
+                //If the player's required food level (amount to feed) is larger than what the food item has,
+                //use the remaining notches of food to fill the player
+                if (amountToFeed > remainingFoodNotches) {
+                    player.getFoodData().setFoodLevel(playerFoodLevel + remainingFoodNotches);
+                    if (playerFoodLevel >= 20) {
+                        player.getFoodData().setFoodLevel(20);
+                    }
+
+                    //Fill the player's food saturation 1.4x the amount that was fed to the player.
+                    amountToSaturate = (float) (remainingFoodNotches * 1.4);
+                    if (amountToSaturate > playerFoodLevel + remainingFoodNotches) {
+                        amountToSaturate = playerFoodLevel + remainingFoodNotches;
+                    }
+                    player.getFoodData().setSaturation(playerSaturationLevel + amountToSaturate);
+
+                    remainingFoodNotches = 0;
+
+                } else {
+                    //If the amountToFeed is larger than the remaining Notches, just fill the player to full.
+                    player.getFoodData().setFoodLevel(20);
+
+                    //Fill the player's food saturation 1.4x the amount that was fed to the player.
+                    amountToSaturate = (float) (amountToFeed * 1.4);
+                    if (amountToSaturate > playerFoodLevel + amountToFeed) {
+                        amountToSaturate = playerFoodLevel + amountToFeed;
+                    }
+                    player.getFoodData().setSaturation(playerSaturationLevel + amountToSaturate);
+                    remainingFoodNotches -= amountToFeed;
+                }
+
+                //Save the new nbt
+                setTag(item, FOOD_NOTCHES_TAG, remainingFoodNotches);
+
+                //Calculate the damage percentage.
+                int chunkusDamageAmount = (int) (100 - (((double) remainingFoodNotches / (double) maxFoodNotches) * 100));
+                if (remainingFoodNotches != maxFoodNotches && chunkusDamageAmount == 0) { chunkusDamageAmount = 1; }
+
+                //Damage the item
+                item.setDamageValue(chunkusDamageAmount);
+            }
+
+            //"Break" the item if it has no more food notches
+            if (remainingFoodNotches <= 0) {
+                return itemstack;
+            } else {
+                return item;
+            }
         } else {
-            return item;
+            //Entity is not a player, delete the item if eaten.
+            return itemstack;
         }
+    }
+
+    private static void setTag(ItemStack item, String key, int value) {
+        CompoundTag tag = item.getTag();
+        tag.putInt(key,value);
+        item.setTag(tag);
     }
 }
